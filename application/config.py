e6ad836150ca066b32bb00a03cb4a1223d730317
Matt Stanley <stanley.t.matthew@gmail.com>
2017-07-17 19:18:42 -0400

    moves application to central twender directory

--- application/config.py
+++
@@ -1,9 +0,0 @@
-"""
-config.py
-
-contains configuration things for twender web application
-"""
-
-# wtf forms stuff for security
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
--- application/run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""
-run.py
-
-used to start webserver for twender website
-"""
-
-from app import app
-app.run(debug=True)
