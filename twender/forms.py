b8613d44a4012694d7f1291025782a42769c6e63
Matt Stanley <stanley.t.matthew@gmail.com>
2017-07-19 15:29:05 -0400

    updates deprecated flask.ext.__ import

--- twender/forms.py
+++ twender/forms.py
@@ -1,11 +1,11 @@
 """
 forms.py
 
-contains form classes for form objects used in twender 
+contains form classes for form objects used in twender
 web application
 """
 
-from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
