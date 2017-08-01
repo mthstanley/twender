bab00571e5a9198a86aa9be71a52953eff3b9713
Matt Stanley <stanley.t.matthew@gmail.com>
2017-08-01 16:44:14 -0400

    adds cli functions for generating valid names and collecting tweets

--- twender/__init__.py
+++ twender/__init__.py
@@ -1,11 +1,17 @@
+import sys
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_pymongo import PyMongo
 from werkzeug.utils import find_modules, import_string
 from config import config
 import tweepy
+import click
+from bson.objectid import ObjectId
 
-from utils.analysis.learnyouaclassifier import TwenderClassifier
+from utils.learnyouaclassifier import TwenderClassifier
+from utils.tweepystreamer import TwenderStreamListener
+from utils.names import valid_names
 
 bootstrap = Bootstrap()
 mongo = PyMongo()
@@ -33,7 +39,39 @@ def register_cli(app):
     description:
         register all flask command line tasks
     """
-    pass
+    @app.cli.command()
+    @click.argument('src', nargs=-1)
+    @click.option('--threshold', default=50, type=float,
+            help='Accept name/gender label if the percentage occurence of that pair is higher than threshold')
+    @click.option('--save/--no-save', default=False, help='Save valid names to database')
+    def mkvalidnames(src, threshold, save):
+        names = valid_names(list(src), threshold)
+        print(names)
+        if save:
+            existing_names = mongo.db.validnames.find_one()
+            if existing_names is not None:
+                mongo.db.validnames.delete_one({'_id': ObjectId(existing_names['_id'])})
+            mongo.db.validnames.insert_one(names)
+
+
+    # TODO add solution for valid names
+    @app.cli.command()
+    @click.option('--numtweets', default=25, help='Number of tweets to collect')
+    @click.option('--save/--no-save', default=False, help='Save collected names to database')
+    def collecttweets(numtweets, save):
+        with app.app_context():
+            valid_names = mongo.db.validnames.find_one()
+
+        listener = TwenderStreamListener(app.tweepy_api,
+                mongo.db, valid_names, tweet_limit=numtweets)
+        listener.save_to_db = save
+
+        # create streamer, mixin our custom stream listener
+        stream = tweepy.Stream(auth=app.tweepy_api.auth,
+                listener=listener)
+
+        # get all geotagged tweets that are in english
+        stream.filter(locations=[-180,-90,180,90], languages=['en'])
 
 
 def register_blueprints(app):
